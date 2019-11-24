8d50c8c5664b586995cb99531260dc3aeb6be0c1
Stevenhyh1 <yihe_hua@outlook.com>
2019-11-24 11:34:11 -0500

    transfer

--- compute_classifier_feature.py
+++ compute_classifier_feature.py
@@ -28,6 +28,7 @@ obs_len = 20
 pred_len = 30
 
 # following must be changed according to situation !!!!!!!!!
+# foldername = ['001', '002', '003','004', '005']
 foldername = ['001', '002', '003','004', '005', '006', '007',  '008', '009',  '010',  '011', '012',  '013',  '014',  '015',  '016',  '017',  '018',  '019',  '020',  '021']
 data_dir = "../data/train/data"
 batch_feature_dir = "../data/train/data"
